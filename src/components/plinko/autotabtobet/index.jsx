1b5a1137ea2b0305d9ff1068d8311d97f1e9f3aa
Ashraf <ashraf.siddiqui@websultanate.com>
2019-07-24 19:56:17 +0530

    Added sidebar section in plinko game

---
+++ src/components/plinko/autotabtobet/index.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {Input,Button, Divider,Select} from 'antd';
+const { Option } = Select;
+class AutoTabToBet extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="auto_tab manual_tab">
+                <>
+                <label>Bet Amount</label>
+                <div className="total_bet">
+                <Input defaultValue="0.00000000" size={'large'} readOnly={true}/>
+                <Button size={'large'}>½</Button>
+                <Divider type={"vertical"} /> 
+                <Button size={'large'}>2×</Button>        
+                </div>
+                </>
+                <>
+                <label>Risk</label>
+                <div className="total_bet">
+                <Select defaultValue="Low" className="custom-select">
+                <Option value="Low">Low</Option>
+                <Option value="Medium">Medium</Option>
+                <Option value="High">High</Option>
+                </Select>        
+                </div>
+                </>
+                <>
+                <label>Rows</label>
+                <div className="total_bet">
+                <Select defaultValue="8" className="custom-select">
+                <Option value="8">8</Option>
+                <Option value="9">9</Option>
+                <Option value="10">10</Option>
+                <Option value="11">11</Option>
+                <Option value="12">12</Option>
+                <Option value="13">13</Option>
+                <Option value="14">14</Option>
+                <Option value="15">15</Option>
+                <Option value="16">16</Option>
+                </Select>        
+                </div>
+                </>
+                <>
+                <label>Number of Bets</label>
+                <div className="total_bet">
+                <Input defaultValue="0" size={'large'} type='number'/>       
+                </div>
+                </>
+               
+                <>
+                <Button className="bet-btn" block size={'large'}>Start Autobet</Button>
+                </>
+            </div>
+         );
+    }
+}
+export default AutoTabToBet;
--- src/components/plinko/index.jsx
+++ src/components/plinko/index.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import {Row,Col,Tabs} from 'antd';
+import ManualTabToBet from './manualtabtobet';
+import AutoTabToBet from './autotabtobet';
 import './index.css';
+const { TabPane } = Tabs;
 class Plinko extends Component {
     constructor(props) {
         super(props);
@@ -8,8 +12,22 @@ class Plinko extends Component {
     render() { 
         return ( 
             <div className="plinko">
-                plinko
-            </div>
+            <Row>
+               <Col className="main_section" span={18} push={6}  gutter={16}>
+                  plinko game section
+               </Col>
+               <Col span={6} pull={18}>
+               <Tabs defaultActiveKey="manual" size={'small'} className="plinko_tab">
+               <TabPane tab="Manual" key="manual">
+                   <ManualTabToBet {...this.state.manual} handlesqueezechecked={this.handleSqueezeChecked}/>
+               </TabPane>
+               <TabPane tab="Auto" key="auto">
+                  <AutoTabToBet  {...this.state.auto} on_change_win={this.onChangeWin} on_change_loss={this.onChangeLoss}/>
+               </TabPane>
+               </Tabs>
+               </Col>
+               </Row>
+           </div>    
          );
     }
 }
